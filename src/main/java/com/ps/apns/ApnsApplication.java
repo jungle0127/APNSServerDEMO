08ab07474c9cc6f1854f9ff3eb1fce07a792314f
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2018-01-29 18:16:10 +0800

    initial project

---
+++ src/main/java/com/ps/apns/ApnsApplication.java
@@ -0,0 +1,12 @@
+package com.ps.apns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnsApplication.class, args);
+	}
+}
